79fda834e98622a843885abeed37290802e0c4cc
s9407789 <71767052+s9407789@users.noreply.github.com>
2025-07-15 10:59:24 +0800

    Create d064.C

---
+++ d064.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int i;
+    scanf("%d", &i);
+    if ( i % 2 == 1) {
+        printf("Odd\n");
+    } else {
+        printf("Even\n");
+    }
+    return 0;
+}
