08d711afdc9ceb70f39867959d3307eb8e416130
s9407789 <71767052+s9407789@users.noreply.github.com>
2025-07-15 14:29:40 +0800

    Create d071.C

---
+++ d071.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int y;
+    while (scanf("%d", &y) != EOF) {
+        if (y % 400 == 0 || y % 4 == 0 && y % 100 != 0) {
+            printf("a leap year\n");
+        } else {
+            printf("a normal year\n");
+        }
+    }
+    return 0;
+}
