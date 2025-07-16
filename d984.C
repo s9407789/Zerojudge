38a8351d6e66b62be047cb5eb4713782f47a5790
s9407789 <71767052+s9407789@users.noreply.github.com>
2025-07-16 19:13:31 +0800

    Create d984.C

---
+++ d984.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main () {
+    unsigned int a, b, c;
+    while (scanf("%d %d %d", &a, &b, &c) != EOF) {
+        if (a > b + c || c > a && a > b && a + b > c || b > a && a > c && a + c > b) {
+            printf("A\n");
+        } else if (b > a + c || c > b && b > a && b + a > c || a > b && b > c && b + c > a) {
+            printf("B\n");
+        } else {
+            printf("C\n");
+        }
+    }
+    return 0;
+}
